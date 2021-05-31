54de6a911970bb6132a195f98ec6136e6b84c538
jereMYltar <jereMYltar@users.noreply.github.com>
2021-05-31 06:53:02 +0200

    add form and form validator

--- frontend/src/cart.js
+++ frontend/src/cart.js
@@ -85,7 +85,7 @@ function displayModels(cart, products, id)
 
 function displayQuantities ()
 {
-    document.querySelectorAll(`input`).forEach((elt) =>
+    document.querySelectorAll(`#cart input`).forEach((elt) =>
     {
         let source = elt.parentElement.parentElement.id;
         elt.value = getQuantity(storage.get("cart"), getId(source), getModel(source));
@@ -117,7 +117,7 @@ function listen (products)
 
 function listenInputsChange (products)
 {
-    document.querySelectorAll('input').forEach((elt) =>
+    document.querySelectorAll('#cart input').forEach((elt) =>
     {
         elt.addEventListener('change', (e) =>
         {
@@ -226,3 +226,45 @@ function calculateSubtotal(unitPrice, qty)
 {
     return (unitPrice * qty / 100).toFixed(2);
 }
+
+// Form manager
+
+let formInstructions = {
+    "firstName" : "! Le prénom ne peut contenir que des lettres, des espaces ou des traits d'union (-).",
+    "lastName" : "! Le nom ne peut contenir que des lettres, des espaces ou des traits d'union (-).",
+    "telNumber" : "! Le numéro de téléphone doit être au format +33123456789 ou 0123456789 (avec ou sans espace/point/trait d'union entre les paires de chiffre).",
+    "email" : "! L'adresse mail doit être au format texte@domaine.ext",
+    "adress" : "! L'adresse ne peut contenir que des lettres, des espaces, des traits d'union (-), des chiffres, des virgules, des points ou des apostrophes.",
+    "complement" : "! Le complément d'adresse ne peut contenir que des lettres, des espaces, des traits d'union (-), des chiffres, des virgules, des points ou des apostrophes.",
+    "zipCode" : "! Le code postal doit être composé de 5 chiffres.",
+    "city" : "! Le nom de ville ne peut contenir que des lettres, des espaces ou des traits d'union (-)."
+};
+
+let formRegExp = {
+    "firstName" : /^[\s\p{L}-]+$/u,
+    "lastName" : /^[\s\p{L}-]+$/u,
+    "telNumber" : /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s\.-]*\d{2}){4}$/,
+    "email" : /^(?:[\w\.-]+)@(?:[\w]+)\.(?:[a-zA-Z]{2,4})$/i,
+    "adress" : /^[\s\d\x27\x2c\.\p{L}-]+$/u, //accepte les espaces (\s), les nombres (\d), les apostrophes (\x27), les virgules (\x2c), les points (\.), les lettres dont celles accentuées (\p{L}) et les tirets (-)
+    "complement" : /^[\s\d\u{0027}\p{L}-]+$/u,
+    "zipCode" : /^[0-9]{5}$/,
+    "city" : /^[\s\p{L}-]+$/u
+};
+
+let formInputs = document.querySelectorAll("form input");
+formInputs.forEach((input) => {
+    input.addEventListener("change", () => {
+        if (formRegExp[input.id].test(input.value))
+        {
+            console.log(`Le champ ${input.id} est correct.`);
+            input.classList.remove("error");
+            input.nextElementSibling.innerText = "ok";
+        }
+        else
+        {
+            console.log(`Le champ ${input.id} ne correspond pas à la règle : ${formInstructions[input.id]}`);
+            input.classList.add("error");
+            input.nextElementSibling.innerText = formInstructions[input.id];
+        }
+    });
+});
