c9e4b7f9015b7c508162911b8521386226e63ffd
jereMYltar <jereMYltar@users.noreply.github.com>
2021-07-05 23:02:39 +0200

    correction d'un bug sur le contrôle de saisie des champs d'adresse

--- frontend/src/JS/form.js
+++ frontend/src/JS/form.js
@@ -5,7 +5,7 @@ let formInstructions = {
     "lastName" : "! Le nom ne peut contenir que des lettres, des espaces ou des traits d'union (-).",
     "telNumber" : "! Le numéro de téléphone doit être au format +33123456789 ou 0123456789 (avec ou sans espace/point/trait d'union entre les paires de chiffre).",
     "email" : "! L'adresse mail doit être au format texte@domaine.ext",
-    "adress" : "! L'adresse ne peut contenir que des lettres, des espaces, des traits d'union (-), des chiffres, des virgules, des points ou des apostrophes.",
+    "address" : "! L'adresse ne peut contenir que des lettres, des espaces, des traits d'union (-), des chiffres, des virgules, des points ou des apostrophes.",
     "complement" : "! Le complément d'adresse ne peut contenir que des lettres, des espaces, des traits d'union (-), des chiffres, des virgules, des points ou des apostrophes.",
     "zipCode" : "! Le code postal doit être composé de 5 chiffres.",
     "city" : "! Le nom de ville ne peut contenir que des lettres, des espaces ou des traits d'union (-)."
@@ -16,7 +16,7 @@ let formRegExp = {
     "lastName" : /^[\s\p{L}-]+$/u,
     "telNumber" : /^(?:(?:\+|00)33|0)\s*[1-9](?:[\s\.-]*\d{2}){4}$/,
     "email" : /^(?:[\w\.-]+)@(?:[\w]+)\.(?:[a-zA-Z]{2,4})$/i,
-    "adress" : /^[\s\d\x27\x2c\.\p{L}-]+$/u, //accepte les espaces (\s), les nombres (\d), les apostrophes (\x27), les virgules (\x2c), les points (\.), les lettres dont celles accentuées (\p{L}) et les tirets (-)
+    "address" : /^[\s\d\x27\x2c\.\p{L}-]+$/u, //accepte les espaces (\s), les nombres (\d), les apostrophes (\x27), les virgules (\x2c), les points (\.), les lettres dont celles accentuées (\p{L}) et les tirets (-)
     "complement" : /^[\s\d\u{0027}\p{L}-]+$/u,
     "zipCode" : /^[0-9]{5}$/,
     "city" : /^[\s\p{L}-]+$/u
